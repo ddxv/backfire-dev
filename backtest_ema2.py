334a341fe1f4f9f1492a5ad90902fb374ac7057a
james <james@bubbleye.com>
2018-05-08 20:46:19 +0800

    updated git ignore

--- backtest_ema2.py
+++ backtest_ema2.py
@@ -52,29 +52,28 @@ def run_backtest(df, desired_outputs, bt):
     bal = AccountBalances()
     bal.set_usd(bt.principle_usd)
     fills = []
-    for row in list(zip(df['time'], df['high'], df['low'], df['buy_signal'], df['sell_signal'])):
-        if row[3] == 1 and bal.usd > bt.min_usd:
+    for row in list(zip(df['time'], df['close'], df['buy_signal'], df['sell_signal'])):
+        if row[2] == 1 and bal.usd > bt.min_usd:
+            price = row[1]
             value_usd = bt.buy_amt_usd
-            value_btc = (value_usd / row[1])
+            value_btc = value_usd / price
             bal.sub_usd(value_usd)
             bal.add_btc(value_btc)
-            fills.append(create_fill(row[0], 'buy', row[1], value_btc, value_usd))
-        if row[4] == 1 and bal.btc > bt.min_btc:
+            fills.append(create_fill(row[0], 'buy', price, value_btc, value_usd))
+        if row[3] == 1 and bal.btc > bt.min_btc:
+            price = row[1]
             value_btc = bal.btc * bt.sell_pct_btc
-            value_usd = row[2] * bt.sell_pct_btc
+            value_usd = price * bt.sell_pct_btc
             bal.add_usd(value_usd)
             bal.sub_btc(value_btc)
-            fills.append(create_fill(row[0], 'sell', row[2], value_btc, value_usd))
+            fills.append(create_fill(row[0], 'sell', price, value_btc, value_usd))
     num_fills = len(fills)
-    p_btc = bt.principle_usd / df[0:1]['close'].values[0]
-    p_usd = p_btc * df.tail(1)[0:1]['close'].values[0]
-    hodl_roi = (p_usd - bt.principle_usd) / bt.principle_usd
     total_roi = (bal.usd - bt.principle_usd) / bt.principle_usd
-    uplift = (total_roi - hodl_roi) / hodl_roi
-    result = {"sd": df[0:1]['time'].values[0], "ed": df.tail(1)[0:1]['time'].values[0],
-            "roi": total_roi,
-            "usd_bal": bal.usd, "fills": num_fills,
-            "hodl_roi": hodl_roi, "uplift": uplift
+    result = {
+            "usd_bal": bal.usd, "btc_bal": bal.btc, "n_fills": num_fills,
+            "upper_window": bt.upper_window, "lower_window": bt.lower_window,
+            "upper_factor": bt.factor_high, "lower_factor": bt.factor_low,
+            "sell_pct_btc": bt.sell_pct_btc, "buy_amt_usd": bt.buy_amt_usd,
             }
     if desired_outputs == "both":
         fills = pd.DataFrame(fills)
@@ -104,6 +103,19 @@ def prep_data(raw_data, start_time, end_time):
 
 
 def single_backtest(df, bt):
+    final_close = df.tail(2)[0:1]['close'].values[0]
+    hodl_usd = (bt.principle_usd / df[0:1]['close'].values[0]) * final_close
+    hodl_roi = (hodl_usd - bt.principle_usd) / bt.principle_usd
+    df = set_signals(df, bt)
+    results, my_fills = run_backtest(df, 'both', bt)
+    results['hodl_roi'] = hodl_roi
+    results['sd'] = df.time.min()
+    results['ed'] = df.time.max()
+    results['final_bal'] = results['usd_bal'] + (final_close * results['btc_bal'])
+    results['roi'] = (results['final_bal'] - bt.principle_usd) / bt.principle_usd
+    return(df, results, my_fills)
+
+def set_signals(df, bt):
     # When adjust is False, weighted averages are calculated recursively as:
     # weighted_average[i] =  alpha * arg[i] + (1 - alpha) * weighted_average[i - 1]
     upper_alpha = 2 / (bt.upper_window + 1)
@@ -112,7 +124,22 @@ def single_backtest(df, bt):
     df['lower_ema'] = (df.close * bt.factor_low).ewm(alpha = lower_alpha, adjust = False).mean()
     df['buy_signal'] = np.where(((df.close < df.lower_ema) & (df.close.shift(1) > df.lower_ema.shift(1))), 1, 0)
     df['sell_signal'] = np.where(((df.close > df.upper_ema) & (df.close.shift(1) < df.upper_ema.shift(1))), 1, 0)
-    my_results, my_fills = run_backtest(df, 'both', bt)
-    return(df, my_results, my_fills)
+    return(df)
+
+def run_multi(df, my_result_type, bt, my_data):
+    factor_low = 1 - my_data[0]
+    factor_high = 1 + my_data[1]
+    bt.set_factor_low(factor_low)
+    bt.set_factor_high(factor_high)
+    bt.set_sell_pct_btc(my_data[2])
+    bt.set_buy_amt_usd(my_data[3])
+    bt.set_lower_window(my_data[4])
+    bt.set_upper_window(my_data[5])
+    df = set_signals(df, bt)
+    df = df[(df['sell_signal']==1) | (df['buy_signal'] == 1)]
+    result = run_backtest(df, my_result_type, bt)
+    #result = {**pre_res_dict, **result}
+    #uplift = (total_roi - hodl_roi) / hodl_roi
+    return(result)
 
 
