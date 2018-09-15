6fb7b9e4dae00b604a9ecf6f14864cb290f98905
judasbenhur <m@tobiasbaumann.net>
2018-09-15 08:42:22 +0000

    WIP: new itertuples &  functions

--- backfire/test/ema.py
+++ backfire/test/ema.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from datetime import timedelta
 import logging
 import numpy as np
+import time
+import sys
+import itertools
 logger = logging.getLogger(__name__)
 
 class AccountBalances:
@@ -98,23 +101,40 @@ def run_backtest(df, desired_outputs, bt):
     """
     bal = AccountBalances(bt.principle_btc, bt.principle_usd)
     fills = []
-    for row in list(zip(df['timestamp'], df['close'], df['buy_signal'], df['sell_signal'])):
+    #for row in list(zip(df['timestamp'], df['close'], df['buy_signal'], df['sell_signal'])):
+    for row in df.itertuples(index=False):
         price = row[1]
+        # BUY SIGNAL
         if row[2] == 1 and ((bal.usd * bt.buy_pct_usd) > bt.min_usd):
-            value_usd = bal.usd * bt.buy_pct_usd
-            value_btc = value_usd / price
-            value_btc = value_btc - (value_btc * .001)
-            bal.btc += value_btc
-            bal.usd -= value_usd
-            fills.append(create_fill(row[0], 'buy', price, value_btc, value_usd))
+            usd_spend_amt = bal.usd * bt.buy_pct_usd
+            btc_receive_amt = usd_spend_amt / price
+            btc_receive_amt = btc_receive_amt - (btc_receive_amt * .001)
+            bal.btc += btc_receive_amt
+            bal.usd -= usd_spend_amt
+            running_bal = bal.usd + (bal.btc * price)
+            fills.append(create_fill(row[0], 'buy', price, btc_receive_amt, usd_spend_amt, running_bal))
+        # SELL SIGNAL
         if row[3] == 1 and (bal.btc * bt.sell_pct_btc) > bt.min_btc:
-            value_btc = bal.btc * bt.sell_pct_btc
-            value_usd = price * value_btc
-            value_usd = value_usd - (value_usd * .001)
-            bal.usd += value_usd
-            bal.btc -= value_btc
-            fills.append(create_fill(row[0], 'sell', price, value_btc, value_usd))
+            btc_spend_amt = bal.btc * bt.sell_pct_btc
+            usd_receive_amt = price * btc_spend_amt
+            usd_receive_amt = usd_receive_amt - (usd_receive_amt * .001)
+            bal.usd += usd_receive_amt
+            bal.btc -= btc_spend_amt
+            running_bal = bal.usd + (bal.btc * price)
+            fills.append(create_fill(row[0], 'sell', price, btc_spend_amt, usd_receive_amt, running_bal))
     num_fills = len(fills)
+
+    # draw down calculation:
+    if desired_outputs == 'backtest':
+        fills = pd.DataFrame(fills)
+        # enough fills to calculate it?
+        if len(fills) > 0:
+            dd_max_usd, dd_duration, dd_percent = create_drawdowns(fills['running_bal'])
+        else:
+            dd_max_usd = 0
+            dd_duration = 0
+            dd_percent = 0
+
     result = {
             "n_fills": num_fills,
             "upper_window": bt.upper_window,
@@ -124,22 +144,73 @@ def run_backtest(df, desired_outputs, bt):
             "buy_pct_usd": bt.buy_pct_usd,
             "sell_pct_btc": bt.sell_pct_btc,
             "usd_bal": bal.usd,
-            "btc_bal": bal.btc
+            "btc_bal": bal.btc,
+            "drawdown": dd_percent,
+            "dd_duration": dd_duration
             }
     if desired_outputs == "both":
-        fills = pd.DataFrame(fills)
+#        fills = pd.DataFrame(fills)
         return result, fills
     else:
         return result
 
 
-def create_fill(my_time, my_side, btc_price, btc_val, usd_val):
+def run_backtest_reduced(df, bt):
+    """
+    Same as above but more reduced for faster iteration of millions of tests
+    """
+    bal = AccountBalances(bt.principle_btc, bt.principle_usd)
+    fills = []
+    n_buys = 0
+    n_sells = 0
+    #for row in list(zip(df['timestamp'], df['close'], df['buy_signal'], df['sell_signal'])):
+    for row in df.itertuples(index=False):
+        price = row[1]
+        # BUY SIGNAL
+        if row[2] == 1 and ((bal.usd * bt.buy_pct_usd) > bt.min_usd):
+            usd_spend_amt = bal.usd * bt.buy_pct_usd
+            btc_receive_amt = usd_spend_amt / price
+            btc_receive_amt = btc_receive_amt - (btc_receive_amt * .001)
+            bal.btc += btc_receive_amt
+            bal.usd -= usd_spend_amt
+            running_bal = bal.usd + (bal.btc * price)
+            n_buys += 1
+        # SELL SIGNAL
+        if row[3] == 1 and (bal.btc * bt.sell_pct_btc) > bt.min_btc:
+            btc_spend_amt = bal.btc * bt.sell_pct_btc
+            usd_receive_amt = price * btc_spend_amt
+            usd_receive_amt = usd_receive_amt - (usd_receive_amt * .001)
+            bal.usd += usd_receive_amt
+            bal.btc -= btc_spend_amt
+            running_bal = bal.usd + (bal.btc * price)
+            n_sells += 1
+    num_fills = n_buys + n_sells
+
+
+    result = {
+            "n_fills": num_fills,
+            "n_buys": n_buys,
+            "n_sells": n_sells,
+            "upper_window": bt.upper_window,
+            "lower_window": bt.lower_window,
+            "upper_factor": bt.factor_high,
+            "lower_factor": bt.factor_low,
+            "buy_pct_usd": bt.buy_pct_usd,
+            "sell_pct_btc": bt.sell_pct_btc,
+            "usd_bal": bal.usd,
+            "btc_bal": bal.btc
+            }
+    return result
+
+
+def create_fill(my_time, my_side, btc_price, btc_val, usd_val, running_bal):
     result = {}
     result['timestamp'] = my_time
     result['side'] = my_side
     result['price'] = btc_price
     result['btc_val'] = btc_val
     result['usd_val'] = usd_val
+    result['running_bal'] = running_bal
     return(result)
 
 # returns a time in the past with correct offset so the ema line can calculate correctly before our
@@ -162,15 +233,24 @@ def prep_data(raw_data, start_time, end_time):
     day_df = trimmed_df.groupby(trimmed_df['timestamp'].dt.date).agg({'open':'first',  'high': max, 'low': min, 'close':'last',}).reset_index()
     return(day_df, trimmed_df)
 
+def prep_data_old(raw_data, start_time, end_time):
+    #raw_data = pd.read_csv('~/coinbase_data.csv')
+    raw_data['timestamp'] = pd.to_datetime(raw_data.timestamp)
+    trimmed_df = raw_data[raw_data.timestamp >= start_time]
+    trimmed_df = trimmed_df[trimmed_df.timestamp <= end_time]
+    trimmed_df = trimmed_df.reset_index(drop = True)
+    day_df = trimmed_df.groupby(trimmed_df['timestamp'].dt.date).agg({'open':'first',  'high': max, 'low': min, 'close':'last',}).reset_index()
+    return(day_df, trimmed_df)
+
 def add_vectorized_cols(df, results, bt_vars):
-    results['sd'] = bt_vars.start_date
-    results['ed'] = bt_vars.end_date
-    results['p_usd'] = bt_vars.principle_usd
-    results['p_btc'] = bt_vars.principle_btc
+#    results['sd'] = bt_vars.start_date
+#    results['ed'] = bt_vars.end_date
+#    results['p_usd'] = bt_vars.principle_usd
+#    results['p_btc'] = bt_vars.principle_btc
     first_close = df[0:1]['close'].values[0]
     final_close = df.tail(2)[0:1]['close'].values[0]
-    results['open'] = first_close
-    results['close'] = final_close
+#    results['open'] = first_close
+#    results['close'] = final_close
     hodl_btc_total = (bt_vars.principle_usd / first_close) + bt_vars.principle_btc
     total_usd_principle = hodl_btc_total * first_close
     hodl_usd_final = hodl_btc_total * final_close
@@ -178,6 +258,10 @@ def add_vectorized_cols(df, results, bt_vars):
     results['hodl_roi'] = hodl_roi
     results['final_bal'] = results['usd_bal'] + (final_close * results['btc_bal'])
     results['roi'] = (results['final_bal'] - total_usd_principle) / total_usd_principle
+# draw down calculation:
+#dd_max_usd, dd_duration, dd_percent = create_drawdowns(my_fills['running_bal'])
+#results['drawdown'] = dd_percent
+#results['dd_duration'] = dd_duration
     return(results)
 
 
@@ -199,8 +283,11 @@ def single_backtest(df, bt_vars):
     results = add_vectorized_cols(df, results, bt_vars)
     return(df, results, my_fills)
 
+total_left = 0
+time_last_start = 0
+next_print = 0
 
-def run_multi(df, result_type, bt, my_data):
+def run_multi(df, result_type, bt, my_data, totalfl):
     """ run_multi runs parallelized backtests on a iterable my_data
     and sets buy & sell signals.
     These signals are sent as ang df to run_backtest
@@ -218,9 +305,28 @@ def run_multi(df, result_type, bt, my_data):
             order in which values are put into the my_data 
             represents their index
     """
+
+    global total_left    
+    global time_last_start
+    global next_print
+    totalfl = int(totalfl / 4) #divide by 4 because of 4 pool threads
+    total_left = totalfl
+    next_print = totalfl + 1
+    time_last_start = time.time()
+
     res_list = []
-    with Pool(8) as p:
-        my_partial_func = partial(parallelized_backtest, df, result_type, bt)
+    with Pool(4) as p:
+        my_partial_func = partial(parallelized_backtest, df, result_type, bt, totalfl)
+        result_list = p.map(my_partial_func, my_data)
+        res_list.append(result_list)
+    res_df = pd.concat([pd.DataFrame(d) for d in res_list]).reset_index(drop = True)
+    res_df = add_vectorized_cols(df, res_df, bt)
+    return(res_df)
+
+def run_multi_v2(df, bt, my_data):
+    res_list = []
+    with Pool(4) as p:
+        my_partial_func = partial(parallelized_backtest_v2, df, bt)
         result_list = p.map(my_partial_func, my_data)
         res_list.append(result_list)
     res_df = pd.concat([pd.DataFrame(d) for d in res_list]).reset_index(drop = True)
@@ -233,7 +339,25 @@ def ema_crosses(df, lower_window, upper_window):
     df['sell_signal'] = np.where(((df.close > df[upper_window]) & (df.close.shift(1) < df[upper_window].shift(1))), 1, 0)
     return(df)
 
-def parallelized_backtest(df, result_type, bt, my_data):
+
+def parallelized_backtest(df, result_type, bt, totalfl, my_data):
+    """
+    global total_left
+    global time_last_start
+    global next_print
+    total_left -= 1
+    #estimate how much time left
+    
+    if (next_print >= total_left):
+        #seconds_left = ((time.process_time() - time_last_start) / (totalfl - total_left)) * total_left
+        #print('             ', total_left, 'sets remain and', round(seconds_left / 60 , 3), 'minutes left...', end='\r')
+        time_elapsed = time.time() - time_last_start
+        items_calculated = int(totalfl - total_left)
+        time_per_set = time_elapsed / items_calculated
+        sstr = '['+str(total_left)+'/'+str(totalfl)+'] time_elapsed='+str(round(time_elapsed,3))+' items_calculated='+str(items_calculated)+' seconds per set='+str(round(time_per_set, 4))+' estimate minutes left='+str(round((time_per_set*total_left) / 60 , 3))
+        print(sstr, end='\r')
+        next_print -= 1
+    """ 
     bt.upper_window = my_data[0]
     bt.lower_window = my_data[1]
     bt.factor_high = my_data[2]
@@ -248,6 +372,38 @@ def parallelized_backtest(df, result_type, bt, my_data):
     result = run_backtest(df, result_type, bt)
     return(result)
 
+def parallelized_backtest_v2(df, bt, my_data):
+    """
+    global total_left
+    global time_last_start
+    global next_print
+    total_left -= 1
+    #estimate how much time left
+    
+    if (next_print >= total_left):
+        #seconds_left = ((time.process_time() - time_last_start) / (totalfl - total_left)) * total_left
+        #print('             ', total_left, 'sets remain and', round(seconds_left / 60 , 3), 'minutes left...', end='\r')
+        time_elapsed = time.time() - time_last_start
+        items_calculated = int(totalfl - total_left)
+        time_per_set = time_elapsed / items_calculated
+        sstr = '['+str(total_left)+'/'+str(totalfl)+'] time_elapsed='+str(round(time_elapsed,3))+' items_calculated='+str(items_calculated)+' seconds per set='+str(round(time_per_set, 4))+' estimate minutes left='+str(round((time_per_set*total_left) / 60 , 3))
+        print(sstr, end='\r')
+        next_print -= 1
+    """ 
+    bt.upper_window = my_data[0]
+    bt.lower_window = my_data[1]
+    bt.factor_high = my_data[2]
+    bt.factor_low = my_data[3]
+    bt.buy_pct_usd = my_data[4]
+    bt.sell_pct_btc = my_data[5]
+    # trim results and ignore fills below on our start date:
+    upper_window = f'upper_{bt.upper_window}_{bt.factor_high}'
+    lower_window = f'lower_{bt.lower_window}_{bt.factor_low}'
+    df = ema_crosses(df, lower_window, upper_window)
+    df = df[(df['sell_signal'] == 1) | (df['buy_signal'] == 1)]
+    result = run_backtest_reduced(df, bt)
+    return(result)
+
 def fills_running_bal(fills_df, bt):
     if len(fills_df) > 0:
         fills_df['p_usd'] = bt.principle_usd
@@ -258,8 +414,39 @@ def fills_running_bal(fills_df, bt):
                 fills_df['usd_val'] * -1, fills_df['usd_val'])
         fills_df['bal_btc'] = fills_df.btc_val.cumsum() + bt.principle_btc
         fills_df['bal_usd'] = fills_df.usd_val.cumsum() + bt.principle_usd
-        fills_df['running_bal'] = (fills_df['bal_btc'] * fills_df['price']) + fills_df['bal_usd']
+#        fills_df['running_bal'] = (fills_df['bal_btc'] * fills_df['price']) + fills_df['bal_usd']
     else:
         logger.warning(f'Fills DataFrame is empty')
     return(fills_df)
 
+
+def create_drawdowns(equity_curve):
+    """
+    Calculate the largest peak-to-trough drawdown of the PnL curve
+    as well as the duration of the drawdown. Requires that the 
+    pnl_returns is a pandas Series.
+
+    Parameters:
+    pnl - A pandas Series representing period percentage returns.
+
+    Returns:
+    drawdown, duration - Highest peak-to-trough drawdown and duration.
+    """
+
+    # Calculate the cumulative returns curve 
+    # and set up the High Water Mark
+    # Then create the drawdown and duration series
+    hwm = [0]
+    eq_idx = equity_curve.index
+    drawdown = pd.Series(index = eq_idx)
+    duration = pd.Series(index = eq_idx)
+    df_dd = pd.DataFrame(drawdown)
+
+    # Loop over the index range
+    for t in range(1, len(eq_idx)):
+        cur_hwm = max(hwm[t-1], equity_curve[t])
+        hwm.append(cur_hwm)
+        drawdown[t]= hwm[t] - equity_curve[t]
+        duration[t]= 0 if drawdown[t] == 0 else duration[t-1] + 1
+    
+    return drawdown.max(), duration.max(), (drawdown.max() / equity_curve.max())
